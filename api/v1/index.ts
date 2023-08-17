3ca1b292c2da338e8f5b533bc0019f52dd80833a
arvinxx <arvinx@foxmail.com>
2023-08-18 00:48:45 +0800

    🐛 fix: fix plugin error

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -7,7 +7,9 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.status(405);
   }
 
-  const result = await fetchContent(req.body);
+  const data = typeof req.body === 'string' ? JSON.parse(JSON.parse(req.body)) : req.body;
+
+  const result = await fetchContent(data);
 
   res.send(result);
 }
